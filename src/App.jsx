fb1d9ff876137d03536ac9e0be51ae9156308385
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-22 14:09:15 +0200

    update and fix for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <>
+        <Header />
         <Footer />
     </>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="footer bg-neutral text-neutral-content flex justify-center items-center p-4">
             <aside className="grid-flow-col items-center">
                 <i className="fa-solid fa-book-open"></i>
-                <p>Copyright One Line A Day © {new Date().getFullYear()} - All right reserved</p>
+                <p>One Line A Day - Copyright © {new Date().getFullYear()} - All right reserved</p>
             </aside>
         </footer>
     );
