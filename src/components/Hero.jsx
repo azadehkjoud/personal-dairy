00def5884434ddac0b00af467dc71220c5a956eb
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-23 13:31:37 +0200

    added quote to hero section using daisyUI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,10 +31,15 @@ const Hero = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
+                    <h1 className="text-5xl font-bold mb-4">One line a day might light you the way</h1>
                     {quote && (
-                        <p className="mb-5">{quote[0].q}</p>
+                        <div className="chat chat-start absolute right-4 max-w-64 bottom-10">
+                            <div className="chat-header">
+                                {quote[0].a}
+                            </div>
+                            <div className="chat-bubble chat-bubble-accent">{quote[0].q}</div>
+                        </div>
                     )}
-                    <button className="btn btn-primary">Add Entry</button>
                 </div>
             </div>
         </section>
