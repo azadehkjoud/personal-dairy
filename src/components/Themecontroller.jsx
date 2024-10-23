1ed392396200a1a98031f5de65c7d0ca7d21f7d8
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-23 14:59:54 +0200

    added functionality to the theme controller.

--- src/components/Themecontroller.jsx
+++ src/components/Themecontroller.jsx
@@ -8,7 +8,7 @@ const Themecontroller = () => {
 
             {/* sun icon */}
             <svg
-                className="swap-off h-10 w-10 fill-current"
+                className="swap-off h-6 w-6 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
                 <path
@@ -17,7 +17,7 @@ const Themecontroller = () => {
 
             {/* moon icon */}
             <svg
-                className="swap-on h-10 w-10 fill-current"
+                className="swap-on h-6 w-6 fill-current"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24">
                 <path
