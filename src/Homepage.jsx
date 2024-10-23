41ad502eddfbc9ba0b36aa711416ed50be254b30
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-23 14:41:15 +0200

    fix for updateEntry prop

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -2,14 +2,14 @@ import DiaryEntryCard from './components/DiaryEntryCard.jsx';
 import Hero from './components/Hero.jsx';
 
 // This is the main page that will display all the diary entries and handle adding new entries
-const Homepage = ({entries}) => {
+const Homepage = ({entries, updateEntry}) => {
     return (
         <>
             <Hero />
             <main className="p-5 self-center w-full max-w-[1200px]">
                 <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     {entries.map((entry, index) => (
-                        <DiaryEntryCard key={index} entry={entry} />
+                        <DiaryEntryCard key={index} entry={entry} updateEntry={updateEntry}  />
                     ))}
                 </section>
             </main>
