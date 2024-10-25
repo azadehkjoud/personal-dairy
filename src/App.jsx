7d1c7c534074b3a2686340fe42b5c4707b75c76b
René Weiberlenn <rene.weiberlenn@live.de>
2024-10-25 11:12:26 +0200

    reverted some changes, but kept the documentation due to bug, a function that requires the entries state object or references the setEntries function apparently cannot be exported/imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import {useEffect, useState} from 'react';
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
-import {saveLocalStorage} from './modules/storage.js';
-import {addEntry, updateEntry} from './modules/entryManagement.js';
+import {addToStorage, getLocalStorage, removeFromStorage, saveLocalStorage} from './modules/storage.js';
 import Layout from './Layout.jsx';
 import Journal from './pages/Journal.jsx';
 import Homepage from './pages/Homepage.jsx';
@@ -20,6 +19,35 @@ function App() {
         // const storedEntries = getLocalStorage('diaryEntries');
         setEntries(storedEntries);
     }, []);
+
+    /**
+     * Adds a new entry to the localStorage under 'diaryEntries' and updates the local state
+     * @param {Object} newEntry - The entry to be added, containing relevant data fields for a diary entry
+     */
+    const addEntry = (newEntry) => {
+        addToStorage('diaryEntries', newEntry);
+        setEntries(() => getLocalStorage('diaryEntries'));
+    }
+
+    /**
+     * Updates an existing entry in localStorage under 'diaryEntries' by removing the old version and adding the updated one.
+     * Updates the local state to reflect the changes.
+     * @param {Object} updatedEntry - The entry with updated values, replacing the old entry with matching identifier
+     */
+    const updateEntry = (updatedEntry) => {
+        removeFromStorage('diaryEntries', updatedEntry);
+        addToStorage('diaryEntries', updatedEntry);
+        setEntries(() => getLocalStorage('diaryEntries'));
+    }
+
+    /**
+     * Removes an entry from localStorage under 'diaryEntries' and updates the local state
+     * @param {Object} entry - The entry to be removed, identified by unique fields within the entry object
+     */
+    const removeEntry = (entry) => {
+        removeFromStorage('diaryEntries', entry);
+        setEntries(() => getLocalStorage('diaryEntries'));
+    }
 
     const router = createBrowserRouter(
         createRoutesFromElements(
